aa80b4be68292b528ab51e0e5fa8d6aa1f27d807
Rahul4321Kumar <rkumar@deqode.com>
2021-12-06 16:08:02 +0530

    changes done as per today review

--- game_quiz.py
+++ game_quiz.py
@@ -6,10 +6,11 @@ import sys
 
 class Question:
     """ The class based implementation of python quiz game"""
+    
     def __init__(self, prompt, answer):
         self.prompt = prompt
-
         self.answer = answer
+    
     @classmethod
     def greet(cls):
         """Function Introduce user about game"""
@@ -23,10 +24,37 @@ class Question:
         if play != "yes":
             sys.exit()
             
+
+
+
+class StartGame():
+    def __init__(self, questions):
+        self.questions = questions
+
+    def run_quiz(self):
+        """Quiz code started from here"""
+        score = 0.1
+        number_of_question = 0
+        Question.greet()
+        Question.interest()
+        for question in self.questions:
+            answer = input(question.prompt)
+            if answer == question.answer:
+                print("correct")
+                score += 33.33
+                number_of_question += 1
+            else:
+                print("incorrect")
+        print(f"Thankyou for Playing this small quiz game, you attempted {number_of_question} questions correctly!")
+        print(f"Marks obtained: {round(score)}")
+        print("BYE!")
+
+
 question_prompts = {
     "1": "What is your Favourite programming language?",
     "2": "Do you follow any author on AskPython?",
     "3": "What is the name of your favourite website for learning Python?"
+    
 }
 
 q1 =Question(question_prompts["1"], "python")
@@ -35,25 +63,6 @@ q3=Question(question_prompts["3"], "askpython")
 
 questions=[q1, q2, q3]
 
+start=StartGame(questions)
+start.run_quiz()
 
-
-def run_quiz(questions):
-    """Quiz code started from here"""
-    score = 0.1
-    number_of_question = 0
-    Question.greet()
-    Question.interest()
-    for question in questions:
-        answer = input(question.prompt)
-        if answer == question.answer:
-            print("correct")
-            score += 33.33
-            number_of_question += 1
-        else:
-            print("incorrect")
-    print(f"Thankyou for Playing this small quiz game, you attempted {number_of_question} questions correctly!")
-    print(f"Marks obtained: {round(score)}")
-    print("BYE!")
-
-
-run_quiz(questions)
